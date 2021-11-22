4feb1be716467119ef8970afff9efa60f16e7fd8
vdv7105131 <vdv7105131@gmail.com>
2021-11-22 13:09:27 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        //Создайте свой тип исключения.
+
+        //Сделайте массив из пяти различных видов исключений, включая собственный тип исключения.
+        //Реализуйте конструкцию TryCatchFinally, в которой будет итерация на каждый тип исключения (блок finally по желанию).
+
+        //В блоке catch выведите в консольном сообщении текст исключения.
+
+        static void Method1()
+        {
+            try
+            {
+                object[] arrayException = { };
+                arrayException[0] = new ArgumentNullException();
+                arrayException[1] = new RankException();
+                arrayException[2] = new DivideByZeroException();
+                arrayException[3] = new InvalidOperationException();
+                arrayException[4] = new NotImplementedException();
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+            catch (RankException ex)
+            {
+                Console.WriteLine(ex.Source);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (NotImplementedException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+    }
+}
