e28bf0b361223c682751828770b427cf23523884
vdv7105131 <vdv7105131@gmail.com>
2021-11-24 13:00:09 +0300

    ok

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -33,29 +33,70 @@ namespace Task2
         {
             switch (number)
             {
-                case 1: Console.WriteLine("Введено значение: 1"); break;
-                case 2: Console.WriteLine("Введено значение: 2"); break;
+                case 1: Sort1(Users()); break;
+                case 2: Sort2(Users()); break;
             }
         }
-        class NumberRead
+
+        public static string[] Users()
         {
-            public delegate void NumberEnteredDelegate(int number);
-            public event NumberEnteredDelegate NumberEnteredEvent;
+            string[] users = new string[5];
+            users[0] = "Иванов";
+            users[1] = "Петров";
+            users[2] = "Обама";
+            users[3] = "Сидоров";
+            users[4] = "Жириновский";
 
-            public void Read()
+            return users;
+        }
+        public static string[] Sort1(string[] users)
+        {
+            Array.Sort(users);
+
+            foreach (string user in users)
             {
-                Console.WriteLine();
-                Console.WriteLine("Введите значение 1 или 2");
-
-                int number = Convert.ToInt32(Console.ReadLine());
-                if (number != 1 && number != 2) throw new FormatException();
-                NumberEntered(number);
+                Console.WriteLine(user);
             }
 
-            protected virtual void NumberEntered(int number)
+            return users;
+        }
+        public static void Sort2(string[] users)
+        {
+            string[] usersSort = new string[users.Length];
+
+            Array.Sort(users);
+
+            int a = 0;
+            for (int i = users.Length - 1; i >= 0; i--)
             {
-                NumberEnteredEvent?.Invoke(number);
+                usersSort[i] = users[a];
+                a++;
             }
+
+            foreach (string user in usersSort)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+    class NumberRead
+    {
+        public delegate void NumberEnteredDelegate(int number);
+        public event NumberEnteredDelegate NumberEnteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введите значение 1 или 2");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number != 1 && number != 2) throw new FormatException();
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEnteredEvent?.Invoke(number);
         }
     }
 }
