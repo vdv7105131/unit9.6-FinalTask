1918d87e30ba09bfdcd7aa5cac4335a52e925641
vdv7105131 <vdv7105131@gmail.com>
2021-11-26 15:20:11 +0300

    ok

---
+++ Task2/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class NumberRead
+    {
+        public delegate void NumberEnteredDelegate(int number);
+        public event NumberEnteredDelegate NumberEnteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Введите значение 1 или 2");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number != 1 && number != 2) throw new FormatException();
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEnteredEvent?.Invoke(number);
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -79,24 +79,5 @@ namespace Task2
             }
         }
     }
-    class NumberRead
-    {
-        public delegate void NumberEnteredDelegate(int number);
-        public event NumberEnteredDelegate NumberEnteredEvent;
 
-        public void Read()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Введите значение 1 или 2");
-
-            int number = Convert.ToInt32(Console.ReadLine());
-            if (number != 1 && number != 2) throw new FormatException();
-            NumberEntered(number);
-        }
-
-        protected virtual void NumberEntered(int number)
-        {
-            NumberEnteredEvent?.Invoke(number);
-        }
-    }
 }
